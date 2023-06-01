7af9d2173ff35157f0e6ffaf0ab83b8240f79140
Eduardo <Eduardo@DESKTOP-VEC11Q7>
2023-06-01 16:02:58 -0300

    Adicionar arquivos de projeto.

---
+++ HelloWorldSP_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HelloWorldSP_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("HelloWorldSP_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("44A04713-C794-4B65-8AD7-17CE765707E0")]
+[assembly: AssemblyVersion("1.0.0.0")]
